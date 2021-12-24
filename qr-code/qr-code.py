860e827fdd3128c0d79bcf96d9d209048051d917
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2021-12-24 16:25:38 +0500

    fff

--- qr-code/qr-code.py
+++ qr-code/qr-code.py
@@ -1,21 +1,19 @@
 
 import qrcode
 from requests.api import get
-
-def create_qrcode(text, color):
-    qr_code_img = qrcode.QRCode()
-    qr_code_img.add_data(text)
-    canvas = qr_code_img.make_image(fill_color=color)
-    fname = f"qr_code" + ".png"
-    canvas.save(fname, "PNG")
-    return fname
-
-
 import cv2
 from pyzbar.pyzbar import decode
+class Qr:
+    def create_qrcode(text, color):
+        qr_code_img = qrcode.QRCode()
+        qr_code_img.add_data(text)
+        canvas = qr_code_img.make_image(fill_color=color)
+        fname = f"qr_code" + ".png"
+        canvas.save(fname, "PNG")
+        return fname
 
-def get_qrcode(image):
-    img = cv2.imread(image)
-    data = decode(img)[0].data.__str__()
-    return data
+    def get_qrcode(image):
+        img = cv2.imread(image)
+        data = decode(img)[0].data.__str__()
+        return data
 
--- test.py
+++ test.py
@@ -3,3 +3,4 @@ from pytube import YouTube
 
 yt = YouTube("https://www.youtube.com/watch?v=0N2ePiPaUts")
 yt.streams
+
